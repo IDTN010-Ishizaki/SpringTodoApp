c462ca63cb45ae4154042bb40b1f57337e5dc4ec
Kaito Ishizaki <idtn010.ishizaki@gki-edu.com>
2026-06-12 14:32:09 +0900

    バリデーションの見直し

--- src/main/java/com/example/demo/dto/RegisterForm.java
+++ src/main/java/com/example/demo/dto/RegisterForm.java
@@ -1,7 +1,6 @@
 package com.example.demo.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -11,12 +10,10 @@ public class RegisterForm {
     private String id;
 
     @NotBlank(message = "パスワードは必須です")
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[.?/-])[a-zA-Z0-9.?/-]{8,1000}$")
     @Size(min = 8, message = "パスワードは8文字以上にしてください")
     private String password;
 
     @NotBlank(message = "再入力は必須です")
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[.?/-])[a-zA-Z0-9.?/-]{8,1000}$")
     @Size(min = 8, message = "パスワードは8文字以上にしてください")
     private String passwordRetype;
 }
